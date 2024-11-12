4b5c30b9be30784699b79d32fb9ce3e0fba7642f
patel manan <mananlearning26@gmail.com>
2024-11-12 13:30:11 +0530

    first commit

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,59 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void RotationController_Update_m93314EF793FFE5E0D4C3913F8927FEA7F19563F0 (void);
+extern void RotationController__ctor_m6DDEBD58AD4BB610AE85CB5443FE46169FE85BB6 (void);
+extern void SceneController_SwitchScenes_m1A5651FDF5C6E3867D590FEC021B01D7A68DDD56 (void);
+extern void SceneController__ctor_m9716569B18874456A7670484C5001303A31B8907 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	RotationController_Update_m93314EF793FFE5E0D4C3913F8927FEA7F19563F0,
+	RotationController__ctor_m6DDEBD58AD4BB610AE85CB5443FE46169FE85BB6,
+	SceneController_SwitchScenes_m1A5651FDF5C6E3867D590FEC021B01D7A68DDD56,
+	SceneController__ctor_m9716569B18874456A7670484C5001303A31B8907,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	6436,
+	6436,
+	5068,
+	6436,
+	9432,
+	6436,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
